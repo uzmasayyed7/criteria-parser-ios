a4a687a03608a0a0082a2e4abae27bfaa81675c5
uzma <uzma.sayyed@webklipper.com>
2019-12-20 16:55:35 +0530

    repository created

---
+++ CriteriaParser/CriteriaParser/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  CriteriaParser
+//
+//  Created by Uzma Sayyed on 20/12/19.
+//  Copyright © 2019 Uzma Sayyed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ CriteriaParser/CriteriaParser/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  CriteriaParser
+//
+//  Created by Uzma Sayyed on 20/12/19.
+//  Copyright © 2019 Uzma Sayyed. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
